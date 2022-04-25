0dbaec19257ffb9a41fef044bf00fc73934e8a82
TomasKermes <45394822+TomasKermes@users.noreply.github.com>
2022-04-25 17:18:40 +0200

    innit commit, class Person.java

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private String name;
+    private String lastName;
+    private int age;
+
+    public Person(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
