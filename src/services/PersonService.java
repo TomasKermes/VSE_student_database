efeb637384ce03e2366891e3c623c2b2c4b951c5
TomasKermes <45394822+TomasKermes@users.noreply.github.com>
2022-05-08 11:31:49 +0200

    PersonService, querries

--- src/services/PersonService.java
+++ src/services/PersonService.java
@@ -1,15 +1,21 @@
 package services;
 
 import java.io.File;
+import java.io.IOException;
 
 public class PersonService {
     public File file;
 
-    public PersonService(String filepath){
+    public PersonService(String filepath) throws IOException {
 
         File newFile = new File(filepath);
-        if
-        filepath = filepath;
+        if(newFile.exists()){
+            filepath = filepath;
+        }
+        else {
+            throw new IOException("");
+        }
+
     }
 
     public static void writeDataLineByLine(){
